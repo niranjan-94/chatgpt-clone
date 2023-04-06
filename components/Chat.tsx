2886ce4ad001c478a505b8239680992011229d6f
niranjan-94 <learning.niranjan.94@gmail.com>
2023-04-06 19:09:48 +0530

    chat component added

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,10 +1,36 @@
+"use client"
+import { db } from "@/firebase";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+
 type ChatProps = {
   chatId: string;
 }
 
 const Chat = ({ chatId }: ChatProps) => {
+  const {data:session} = useSession();
+  
+  const [message] = useCollection(
+    session && 
+    query(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      orderBy("createdAt", "asc")
+    )
+  )
+  
+  
   return (
-    <div className="flex-1">Chat</div>
+    <div className="flex-1">
+
+    </div>
   )
 }
 
